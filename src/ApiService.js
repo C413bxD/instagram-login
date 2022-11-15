59a14cdd751700e3300708e03526ed26ddd22250
waready <elbarto119@hotmail.com>
2022-11-15 00:51:58 -0500

    arreglos

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,9 +1,9 @@
 import http  from "@/utils/http.js";
 
 class ApiService {
-  static async getUser() {
-    return (await http.get("api/user")).data;
-  }
+//   static async getUser() {
+//     return (await http.get("api/user")).data;
+//   }
 }
 
 export default ApiService;
